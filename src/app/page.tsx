8712981f576d1544b5e086a9bcb2f5ee1b3ce306
shawaalok2003 <aalokshaw2003@gmail.com>
2026-06-02 14:21:26 +0530

    style: remove Featured Opportunities section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { LogoCarousel } from "@/components/landing/LogoCarousel";
 import { JobCarousel } from "@/components/landing/JobCarousel";
 import { HowItWorks } from "@/components/landing/HowItWorks";
 import { Benefits } from "@/components/landing/Benefits";
-import { FeaturedJobs } from "@/components/landing/FeaturedJobs";
 import { Testimonials } from "@/components/landing/Testimonials";
 import { Footer } from "@/components/layout/Footer";
 
@@ -26,7 +25,6 @@ export default async function Home() {
       <JobCarousel jobs={topJobs} />
       <HowItWorks />
       <Benefits />
-      <FeaturedJobs />
       <Testimonials />
       <Footer />
     </main>
